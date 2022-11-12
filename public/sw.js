4091e33ef0439668b6ffe768f90d7783ccbebb45
notAperson535 <notAperson939@gmail.com>
2022-11-12 10:20:45 -0500

    remove useless thing from sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,4 @@ importScripts("./uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
-let userKey = new URL(location).searchParams.get('userkey');
-
 self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
